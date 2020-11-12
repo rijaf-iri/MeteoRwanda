3fdf09becb9ffda0077f78cc70579f3886c31588
Rija Faniriantsoa <rijaf@iri.columbia.edu>
2020-11-11 22:44:14 -0500

    change welcome page

--- app/__init__.py
+++ app/__init__.py
@@ -3,33 +3,32 @@ import datetime
 from flask_jsglue import JSGlue
 
 # Define the WSGI application object
-app = Flask(__name__, instance_relative_config = False)
+app = Flask(__name__, instance_relative_config=False)
 
 # Configurations
-app.config.from_object('config')
+app.config.from_object("config")
 
 jsglue = JSGlue(app)
 
 # HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
-    flash("Welcome Homer Rj!", "info")
-    return render_template("main.html", error_login = False)
+    flash("Welcome to Meteo Rwanda", "info")
+    return render_template("main.html", error_login=False)
+
 
 @app.context_processor
 def utility_processor():
-    def foo():
-        # do something
-        return "return something"
-
-    def date_now(format = "%d.%m.%Y %H:%M:%S"):
+    def date_now(format="%d.%m.%Y %H:%M:%S"):
         return datetime.datetime.now().strftime(format)
 
-    return dict(date_now = date_now, foo = foo)
+    return dict(date_now=date_now)
+
 
 ## Import applications
 from app.mod_auth.usersManagement import mod_auth as auth_module
