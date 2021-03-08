ad339facafda1e98555f84d0dc04d6f058ce6e25
Rija Faniriantsoa <rijaf@iri.columbia.edu>
2021-03-07 22:20:31 -0500

    clean navigation menu

--- app/mod_aws/static/js/params-AWS-QCOutput.js
+++ app/mod_aws/static/js/params-AWS-QCOutput.js
@@ -2,11 +2,15 @@ function setQCOutputTime() {
     var label = ['Year', 'Mon', 'Day', 'Hour'];
     var pname = ['year', 'month', 'day', 'hour'];
     // 
+    $(".ts-start-time").append(selectTimesTsMap(1, ".ts-start-time", label, pname, true));
+    $(".ts-end-time").append(selectTimesTsMap(2, ".ts-end-time", label, pname, false));
     $(".map-select-time").append(selectTimesTsMap(3, ".map-select-time", label, pname, true));
     $('#minute3', '#pentad3', '#dekad3').hide();
 
     //
     var timestep = $("#timestepDispTS option:selected").val();
+    lastDaty = new Date();
+    lastDaty.setDate(lastDaty.getDate() - 30);
     var daty = new Date();
 
     if (timestep == "hourly") {
@@ -21,45 +25,62 @@ function setQCOutputTime() {
         if (i < 10) {
             hr = "0" + i;
         }
-        $('#hour3').append(
+        $('#hour1, #hour2, #hour3').append(
             $("<option>").text(hr).val(hr)
         );
     }
     //
+    // var vhour = daty.getHours();
+    // $("#hour3").val((vhour < 10 ? "0" : "") + vhour);
+    var vhour0 = lastDaty.getHours();
+    $("#hour1").val((vhour0 < 10 ? "0" : "") + vhour0);
     var vhour = daty.getHours();
-    $("#hour3").val((vhour < 10 ? "0" : "") + vhour);
+    $("#hour2, #hour3").val((vhour < 10 ? "0" : "") + vhour);
+
     //
     for (var i = 1; i <= 31; ++i) {
         var dy = i;
         if (i < 10) {
             dy = "0" + i;
         }
-        $('#day3').append(
+        $('#day1, #day2, #day3').append(
             $("<option>").text(dy).val(dy)
         );
     }
+    // var vday = daty.getDate();
+    // $("#day3").val((vday < 10 ? "0" : "") + vday);
+    var vday0 = lastDaty.getDate();
+    $("#day1").val((vday0 < 10 ? "0" : "") + vday0);
     var vday = daty.getDate();
-    $("#day3").val((vday < 10 ? "0" : "") + vday);
+    $("#day2, #day3").val((vday < 10 ? "0" : "") + vday);
+
     //
     for (var i = 1; i <= 12; ++i) {
         var mo = i;
         if (i < 10) {
             mo = "0" + i;
         }
-        $('#month3').append(
+        $('#month1, #month2, #month3').append(
             $("<option>").text(mo).val(mo)
         );
     }
+    // var vmon = daty.getMonth() + 1;
+    // $("#month3").val((vmon < 10 ? "0" : "") + vmon);
+    var vmon0 = lastDaty.getMonth() + 1;
+    $("#month1").val((vmon0 < 10 ? "0" : "") + vmon0);
     var vmon = daty.getMonth() + 1;
-    $("#month3").val((vmon < 10 ? "0" : "") + vmon);
+    $("#month2, #month3").val((vmon < 10 ? "0" : "") + vmon);
     //
     var thisYear = daty.getFullYear();
     for (var yr = 2013; yr <= thisYear; ++yr) {
-        $('#year3').append(
+        $('#year1, #year2, #year3').append(
             $("<option>").text(yr).val(yr)
         );
     }
-    $("#year3").val(thisYear);
+    // $("#year3").val(thisYear);
+    var thisYear0 = lastDaty.getFullYear();
+    $("#year1").val(thisYear0);
+    $("#year2, #year3").val(thisYear);
     //
 
     if (timestep == "hourly") {
